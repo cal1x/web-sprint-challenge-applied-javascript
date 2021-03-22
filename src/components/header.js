8cbc4d2629e070e5eda6787664cb591832b8f674
JuanCalixto <juancalixtomorales@gmail.com>
2021-03-21 19:09:19 -0500

    implement header function to append header

--- src/components/header.js
+++ src/components/header.js
@@ -37,6 +37,7 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  document.querySelector(selector).appendChild(Header('Components', '3/2/2021', '64F'))
 }
 
 export { Header, headerAppender }

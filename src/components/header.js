27fc0553776151ffafd4b5ec8e88a70377073154
JuanCalixto <juancalixtomorales@gmail.com>
2021-03-21 19:04:43 -0500

    add header function elements and attributes

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,23 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const headerDiv = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const titleHeading = document.createElement('h1');
+  const tempSpan = document.createElement('span');
+
+  headerDiv.classList.add('header');
+  dateSpan.classList.add('date');
+  dateSpan.textContent = date;
+  titleHeading.textContent = title;
+  tempSpan.classList.add('temp');
+  tempSpan.textContent = temp;
+
+  headerDiv.appendChild(dateSpan);
+  headerDiv.appendChild(titleHeading);
+  headerDiv.appendChild(tempSpan);
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
